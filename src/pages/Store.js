8031bed2b1dc1ac8050b2de34aaf78d4bace950f
whatthefoobar <irina.mntn@gmail.com>
2023-01-30 10:02:56 +0100

    so far so good

--- src/pages/Store.js
+++ src/pages/Store.js
@@ -15,43 +15,43 @@ const Store = () => {
     <div className="container gallery">
       <div className="img-box">
         <img className="img-sale" src={sale1} alt="sale no 1" />
-        {/* <h5>Test title - 40$</h5>  */}
+        <h5>Test title - 40$</h5>
       </div>
       <div className="img-box">
         <img className="img-sale" src={sale2} alt="" />
-        {/* <h5>Test title</h5>  */}
+        <h5>Test title - 40$</h5>
       </div>
       <div className="img-box">
         <img className="img-sale" src={sale3} alt="" />
-        {/* <h5>Test title - 40$</h5>  */}
+        <h5>Test title - 40$</h5>
       </div>
       <div className="img-box">
         <img className="img-sale" src={sale4} alt="" />
-        {/* <h5>Test title - 40$</h5>  */}
+        <h5>Test title - 40$</h5>
       </div>
       <div className="img-box">
         <img className="img-sale" src={sale5} alt="" />
-        {/* <h5>Test title - 40$</h5>  */}
+        <h5>Test title - 40$</h5>
       </div>
       <div className="img-box">
         <img className="img-sale" src={sale6} alt="" />
-        {/* <h5>Test title - 40$</h5>  */}
+        <h5>Test title - 40$</h5>
       </div>
       <div className="img-box">
         <img className="img-sale" src={sale7} alt="" />
-        {/* <h5>Test title - 40$</h5>  */}
+        <h5>Test title - 40$</h5>
       </div>
       <div className="img-box">
         <img className="img-sale" src={sale8} alt="" />
-        {/* <h5>Test title - 40$</h5>  */}
+        <h5>Test title - 40$</h5>
       </div>
       <div className="img-box">
         <img className="img-sale" src={sale9} alt="" />
-        {/* <h5>Test title - 40$</h5>  */}
+        <h5>Test title - 40$</h5>
       </div>
       <div className="img-box">
         <img className="img-sale" src={sale10} alt="" />
-        {/* <h5>Test title - 40$</h5>  */}
+        <h5>Test title - 40$</h5>
       </div>
     </div>
   );
